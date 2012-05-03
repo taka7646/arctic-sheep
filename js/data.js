f13bfd6f8a45f920a1eddb19557fd1d1d2a1e5cd
Yoshitaka Suzuki <takays@nifty.com>
2012-05-03 14:12:21 +0900

    add new files

---
+++ js/data.js
@@ -0,0 +1,42 @@
+/** アイテムマスタデータ */
+var items = {
+	1:{
+		id: 1,
+		type: 1,
+		name: "りんご",
+		price: 100,
+		image: "img/item/apple.png",
+		discription: "まっかなりんご。けっこうおなかがふくれます",
+	},
+};
+
+/** キャラクタパラメータ */
+var charParams = {
+	1:{
+		id: 1,
+		name: "ひつじ",
+		life: 100,
+		speed: 100,
+		stamina: 100,
+		luck: 100,
+		prize: 1000,
+	},
+	2:{
+		id: 2,
+		name: "うし",
+		life: 100,
+		speed: 80,
+		stamina: 120,
+		luck: 80,
+		prize: 1000,
+	},
+	3:{
+		id: 3,
+		name: "うさぎ",
+		life: 100,
+		speed: 130,
+		stamina: 50,
+		luck: 90,
+		prize: 1000,
+	},
+};
