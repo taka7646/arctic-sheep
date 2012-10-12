fd9a99a145822355f5bc557f06eebb7f363e3d8b
Yoshitaka Suzuki <takays@nifty.com>
2012-10-12 16:55:01 +0900

    add heroku setting

---
+++ server/server.js
@@ -0,0 +1,28 @@
+
+var express = require('express');
+var app = express();
+//var app = express.createServer();
+var http = app.listen(8000);
+var socketio = require('socket.io').listen(http);
+app.configure(function(){
+	app.use(express.static(__dirname + '/..'));
+});
+
+app.get('/', function(req, res) {
+	res.render('index', {});
+});
+
+
+socketio.on('connection', function (socket) {
+	console.log('connected');
+	socket.emit('msg push', {msg:'connected'});
+	
+	socket.on('all', function(msg){
+		socketio.sockets.emit('msg', msg);
+	});
+	
+	socket.on('disconnect', function(){
+		console.log('disconnect');
+	});
+});
+
